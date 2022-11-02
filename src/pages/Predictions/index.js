2a1719fed3f332e8f497db08d8d32d5476416d70
jakerinahmed <jakerin.ahmed2001@gmail.com>
2022-11-02 11:34:04 +0000

    make only one request on predictions page

--- src/pages/Predictions/index.js
+++ src/pages/Predictions/index.js
@@ -1,12 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import { DreamTeam, StatsTable } from '../../components'
 import './style.css'
 
 const Predictions = () => {
+  const [allData, setAllData] = useState([])
+
+  useEffect(() => {
+    async function storePlayerData() {
+      const storeData = await getPlayerData()
+      setAllData(storeData)
+    }
+    storePlayerData()
+  }, [])
+
+  const getPlayerData = async () => {
+    try {
+      const data = await axios.get('https://fantaisyfootball.herokuapp.com/allstats')
+      return data.data
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+
   return (
     <div className='predictions'>
-        <DreamTeam/>
-        <StatsTable/>
+      <DreamTeam />
+      <StatsTable allData={allData} />
     </div>
   )
 }
