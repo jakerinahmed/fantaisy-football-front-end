7a03aeabaea091ec16bd787261238924a82cb6ec
Littlerou <izzymelvin4@gmail.com>
2022-11-02 15:14:59 +0000

    add autherisation

--- src/pages/Predictions/index.js
+++ src/pages/Predictions/index.js
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { DreamTeam, StatsTable } from '../../components'
 import './style.css'
+import axios from 'axios'
 
 const Predictions = () => {
+  let [product, setproduct] = useState([]);
+  let [change, setChange] = useState(true);
+
+async function fetchData() {
+    let response = await axios(
+      `https://fantaisyfootball.herokuapp.com/allstats`
+    );
+    let user = await response.data;
+    setproduct(user);
+    
+    console.log(product);
+    setChange(!change)
+  }
+
+  // useEffect(async () => {
+  //   let response = await axios(
+  //     `https://fantaisyfootball.herokuapp.com/allstats`
+  //   );
+  //   let user = await response.data;
+  //   setproduct(user);
+    
+  //   console.log(product);
+    
+  // },[product]); 
+  
   return (
     <div className='predictions'>
+      <button onClick={() => console.log(product)}>Get stats</button>
         <DreamTeam/>
         <StatsTable/>
     </div>
